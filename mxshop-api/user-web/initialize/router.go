11f094a076149e0b4184265c7eec79c73566913a
zhengpanone <zhengpanone@htomail.com>
2024-03-27 00:25:03 +0800

    跨域问题

--- mxshop-api/user-web/initialize/router.go
+++ mxshop-api/user-web/initialize/router.go
@@ -2,11 +2,14 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"mxshop-api/user-web/middlewares"
 	"mxshop-api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	// 配置跨域
+	Router.Use(middlewares.Cors())
 	ApiGroup := Router.Group("/v1")
 	router.InitUserRouter(ApiGroup)
 	return Router
